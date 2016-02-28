8969d92f37e8df51188f90b1a820b8c65e57411f
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2016-02-27 21:58:54 -0500

    add tests

---
+++ Tests.Framework/System/DecimalTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tests.Framework.System
+{
+	[TestFixture]
+	public class DecimalTests
+	{
+		[Test]
+		public void UncheckedOverflow()
+		{
+			Assert.Throws<OverflowException>(() =>
+			{
+				var value = decimal.MaxValue;
+				var result = unchecked(value + 1);
+			});
+		}
+
+		[Test]
+		public void UncheckedNegativeOverflow()
+		{
+			Assert.Throws<OverflowException>(() =>
+			{
+				var value = decimal.MinValue;
+				var result = unchecked(value - 1);
+			});
+		}
+
+		[Test]
+		public void CheckedUnderflow()
+		{
+			var value = decimal.MinValue;
+			Assert.AreEqual(0, checked(1 / value));
+		}
+	}
+}
---
+++ Tests.Framework/System/DoubleTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Tests.Framework.System
+{
+	[TestFixture]
+	public class DoubleTests
+	{
+		[Test]
+		public void CheckedUnderflow()
+		{
+			var value = double.MinValue;
+			Assert.AreEqual(0, checked(1 / value / value));
+		}
+	}
+}
