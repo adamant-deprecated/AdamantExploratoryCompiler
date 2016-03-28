0d1c3359423bedfd66f496b3890a76b1f14f89ad
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2016-03-28 12:10:00 -0400

    create Compiler.Emit.Cpp project

---
+++ Compiler.Emit.Cpp/PackageEmitter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adamant.Exploratory.Compiler.Compiled;
+
+namespace Compiler.Emit.Cpp
+{
+	public class PackageEmitter
+	{
+		//public PackageEmitter(CompiledPackage )
+	}
+}
